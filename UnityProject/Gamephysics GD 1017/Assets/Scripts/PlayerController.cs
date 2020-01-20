af52bef329155e8cafc83a60155184f6df84dec0
SebastianSyncerski <syncerski.sebastian@yahoo.de>
2020-01-20 14:06:47 +0100

    GravitySphere works yay ich bin Seb ich in der coolste! Oniii-Chan Senapi OwO.

---
+++ UnityProject/Gamephysics GD 1017/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rigid;
+
+    public delegate void CallEvent(SphereScript.CubeMovement cubeMovement, float pullForce, float Mass);
+    public static event CallEvent OnPlayerCall;
+
+    [SerializeField]
+    private float moveSpeed;
+
+
+    [SerializeField]
+    private float pullForce = 10;
+
+    private float Mass;
+
+    private void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+        Mass = rigid.mass;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandlePlayerInput();
+    }
+
+    private void HandlePlayerInput()
+    {
+        Move();
+        Call();
+    }
+    private void Move()
+    {
+        transform.Translate(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * Time.deltaTime * moveSpeed);
+        //rigid.AddForce(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"))* Time.deltaTime * moveSpeed, ForceMode.Impulse);
+    }
+
+    private void Call()
+    {
+        if(Input.GetKey(KeyCode.H))
+        {
+            OnPlayerCall(SphereScript.CubeMovement.towardsPlayer, pullForce, Mass);
+        }
+        else if (Input.GetKey(KeyCode.J))
+        {
+            OnPlayerCall(SphereScript.CubeMovement.awayFromPlayer, pullForce, Mass);
+        } else
+        {
+            OnPlayerCall(SphereScript.CubeMovement.nothing, pullForce, Mass);
+        }
+    }
+}
---
+++ UnityProject/Gamephysics GD 1017/Assets/Scripts/SphereScript.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody))]
+public class SphereScript : MonoBehaviour
+{
+    public enum CubeMovement
+    {
+        towardsPlayer,
+        awayFromPlayer,
+        nothing
+    }
+
+    private CubeMovement currentMoveState = CubeMovement.nothing;
+
+    private Rigidbody rigid;
+
+    private PlayerController player;
+
+    private SphereCollider col;
+
+    private float pullForce;
+
+    private float Radius;
+
+    private float gravitationConstant;
+
+    private float Mass;
+
+    private float massPlayer;
+
+    private float earthSimulationMultiplier;
+
+    private void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+        gravitationConstant = 6.672f * (float)Mathf.Pow(10, -11);
+        col = GetComponent<SphereCollider>();
+        Mass = rigid.mass;
+        earthSimulationMultiplier = Mathf.Pow(10, 10);
+    }
+
+    private void Update()
+    {
+        ApplyMovement();
+    }
+
+    public void ChangeMovement(CubeMovement cubeM, float pullForce, float massPlayer)
+    {
+        this.pullForce = pullForce;
+        this.massPlayer = massPlayer;
+        currentMoveState = cubeM;
+    }
+    private void ApplyMovement()
+    {
+        Vector3 PullVector = (player.transform.position - transform.position).normalized * CalculateGravitationForce();
+        Debug.Log($"pullVector: {PullVector}");
+        switch (currentMoveState)
+        {
+            case CubeMovement.awayFromPlayer:
+                rigid.AddForce(-PullVector, ForceMode.Impulse);
+                break;
+            case CubeMovement.towardsPlayer:
+                rigid.AddForce(PullVector, ForceMode.Impulse);
+                break;
+            case CubeMovement.nothing:
+                break;
+            default:
+                Debug.LogWarning($"undefined cubemovement: {currentMoveState}");
+                break;
+        }
+    }
+    private float CalculateGravitationForce()
+    {
+        Debug.Log($"GravityForce: {((gravitationConstant * Mass * massPlayer * earthSimulationMultiplier) / (player.transform.position - transform.position).magnitude) * pullForce}");
+        return ((gravitationConstant * Mass * massPlayer * earthSimulationMultiplier) / (player.transform.position - transform.position).magnitude) * pullForce;
+    }
+
+
+    private void OnEnable()
+    {
+        PlayerController.OnPlayerCall += ChangeMovement;
+    }
+
+    private void OnDisable()
+    {
+        PlayerController.OnPlayerCall -= ChangeMovement;
+    }
+}
