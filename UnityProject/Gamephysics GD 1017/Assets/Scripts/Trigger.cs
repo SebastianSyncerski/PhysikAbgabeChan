1d0d9f3b8d1d78d661661c142ebebab1532377af
Seb <syncerski.sebastian@yahoo.de>
2020-01-20 17:22:36 +0100

    Projektdokumentation.txt erstellt, mehr physobjekte, mini optimierung

--- UnityProject/Gamephysics GD 1017/Assets/Scripts/Trigger.cs
+++ UnityProject/Gamephysics GD 1017/Assets/Scripts/Trigger.cs
@@ -6,16 +6,23 @@ public class Trigger : MonoBehaviour
 {
     [SerializeField]
     private List<GameObject> ActivateOnTrigger;
+    [SerializeField]
+    private List<GameObject> DeactivateOnTrigger;
 
-    //----if an object with the Sphere script enters the trigger it activates all Items in the ActivateOnTrigger List
     private void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<SphereScript>())
         {
-            Debug.Log("objekt in box ist: " +other);
+            //Debug.Log("objekt in box ist: " +other);
+            //----if an object with the Sphere script enters the trigger it activates all Items in the ActivateOnTrigger List
             foreach (GameObject i in ActivateOnTrigger)
             {
                 i.SetActive(true);
+            }
+            //----if an object with the Sphere script enters the trigger it deactivates all Items in the DeactivateOnTrigger List
+            foreach (GameObject i in DeactivateOnTrigger)
+            {
+                i.SetActive(false);
             }
         }
     }
